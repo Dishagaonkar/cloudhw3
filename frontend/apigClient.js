0beb9cc0c8202269c68ae8f15ab7440526353f0f
Disha Gaonkar <dishagkr@gmail.com>
2025-11-30 11:01:54 -0500

    fixes frontend

--- frontend/apigClient.js
+++ frontend/apigClient.js
@@ -52,8 +52,8 @@ apigClientFactory.newClient = function (config) {
   }
 
   // extract endpoint and path from url
-  // var invokeUrl = 'https://l2ntureec1.execute-api.us-east-1.amazonaws.com/V1';
-  var invokeUrl = "https://bbrnrd1eu8.execute-api.us-east-1.amazonaws.com/V1";
+  var invokeUrl = "https://l2ntureec1.execute-api.us-east-1.amazonaws.com/V1";
+  //   var invokeUrl = "https://bbrnrd1eu8.execute-api.us-east-1.amazonaws.com/V1";
   var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
   var pathComponent = invokeUrl.substring(endpoint.length);
 
